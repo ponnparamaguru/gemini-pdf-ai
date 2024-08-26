dcb5d945b377f13e8e2553be4c63020faad5f351
Ponnparamaguru <ponnparamaguru8@gmail.com>
2024-08-27 01:24:37 +0530

    Update SplashScreen.js

--- client/src/SplashScreen.js
+++ client/src/SplashScreen.js
@@ -10,7 +10,7 @@ const SplashScreen = ({ onHide }) => {
   }, [onHide]);
 
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-white z-50">
+    <div className="bg-teal-500 text-white fixed inset-0 flex items-center justify-center bg-white z-50">
       <div className="text-center">
         <h1 className="text-4xl font-bold mb-2">AI PDF Assisstance</h1>
         <p className="text-lg text-gray-600">Upload PDF and ask question related to this PDF</p>
